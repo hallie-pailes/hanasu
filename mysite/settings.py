bba89ffb8ebee576ed3c879888d740b7f097a7ab
H Pailes <hal6027@gmail.com>
2025-01-02 17:34:14 -0600

    Fix static

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,15 +128,11 @@ USE_TZ = True
 
 #STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "hanasu/static", 
-]
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
